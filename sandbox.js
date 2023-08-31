85696257938ce8c5df25450c291ccd24ee85b032
4theLoveofForms <tangledrootsinfo@gmail.com>
2023-08-31 10:14:24 +0100

    2nd lesson

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,32 @@
-var character = 'nibin';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
-var age = 30;
-var isBlackBelt = false;
-character = 'bob';
-age = 40;
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+var names = ['Alice', 'Bob', 'Eve'];
+// names = 'impossible'; // Type '"impossible"' is not assignable to type 'string[]
+names.push('Mallory');
+// names.push(42); // Error: Argument of type '42' is not assignable to parameter of type 'string'.
+var numbers = [1, 2, 3, 4];
+numbers.push(5);
+var mixed = ['Apple', 1, 'Orange', 2, 'Banana', 3, true];
+mixed.push('Cherry');
+mixed.push(4);
+mixed[0] = 1;
+// objects
+var person = {
+    name: 'Alice',
+    age: 25,
+    belt: 'black',
 };
-console.log(circ(7.5));
+// person.name = 30; // Error: Type '30' is not assignable to type 'string'.
+person.name = 'crazy eyes';
+// person.skills = ['fighting', 'singing']; // Error: Property 'skills' does not exist on type '{ name: string; age: number; belt: string; }'.
+person = {
+    name: 'Bob',
+    age: 30,
+    belt: 'orange',
+};
+// person = {
+//   name: 'Bob',
+//   age: 30,
+//   // belt: 'orange',
+// }; // Error: Type '{ name: string; age: number; }' is not assignable to type '{ name: string; age: number; belt: string; }'.
+// the initial objest sets how future objects will be shaped.  they must have all the same properties as the initial object
+// by default anyway.  I wonder if there is a way to make some optional for example.
+// explicit types
--- sandbox.ts
+++ sandbox.ts
@@ -1,23 +1,44 @@
-let character = 'nibin'
+let names = ['Alice', 'Bob', 'Eve'];
 
-console.log(character)
+// names = 'impossible'; // Type '"impossible"' is not assignable to type 'string[]
 
-const inputs = document.querySelectorAll('input')
+names.push('Mallory');
 
-inputs.forEach(input => {
-    console.log(input)
-});
+// names.push(42); // Error: Argument of type '42' is not assignable to parameter of type 'string'.
+let numbers = [1, 2, 3, 4];
 
-let age = 30;
-let isBlackBelt = false;
+numbers.push(5);
 
-character = 'bob'
+let mixed = ['Apple', 1, 'Orange', 2, 'Banana', 3, true];
 
-age = 40;
-isBlackBelt = true;
+mixed.push('Cherry');
+mixed.push(4);
+mixed[0] = 1;
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI
-}
+// objects
+let person = {
+  name: 'Alice',
+  age: 25,
+  belt: 'black',
+};
 
-console.log(circ(7.5)) 
+// person.name = 30; // Error: Type '30' is not assignable to type 'string'.
+person.name = 'crazy eyes';
+// person.skills = ['fighting', 'singing']; // Error: Property 'skills' does not exist on type '{ name: string; age: number; belt: string; }'.
+
+person = {
+  name: 'Bob',
+  age: 30,
+  belt: 'orange',
+};
+
+// person = {
+//   name: 'Bob',
+//   age: 30,
+//   // belt: 'orange',
+// }; // Error: Type '{ name: string; age: number; }' is not assignable to type '{ name: string; age: number; belt: string; }'.
+
+// the initial objest sets how future objects will be shaped.  they must have all the same properties as the initial object
+// by default anyway.  I wonder if there is a way to make some optional for example.
+
+// explicit types
