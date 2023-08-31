0b82900fbf334a83e51362d332ec9244a49e55a6
4theLoveofForms <tangledrootsinfo@gmail.com>
2023-08-31 10:03:18 +0100

    some first lesson stuff

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+var character = 'nibin';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
+var age = 30;
+var isBlackBelt = false;
+character = 'bob';
+age = 40;
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,23 @@
+let character = 'nibin'
+
+console.log(character)
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    console.log(input)
+});
+
+let age = 30;
+let isBlackBelt = false;
+
+character = 'bob'
+
+age = 40;
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(7.5)) 
