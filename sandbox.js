3460f7dc6b0bf0f7238f888e3720db1665615034
4theLoveofForms <tangledrootsinfo@gmail.com>
2023-08-31 10:32:00 +0100

    declaring array and object .  not very mind blowing

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,4 @@
-// explicit types
-var character;
-var age;
-var isLoggedIn;
-// you can declare types if you don't have a value for them yet
-//arrays 
-var ninjas = [];
-// ninjas = [10, 23] // this will throw an error because ninjas is an array of strings
-ninjas.push('shaun');
-// union types
-var mixed = [];
-mixed.push('hello');
-mixed.push(20);
-// mixed.push(false); // this will throw an error because mixed is an array of strings and numbers 
-var uid;
-uid = '123';
-uid = 123;
-// objects
-var ninjaOne;
-ninjaOne = { name: 'yoshi', age: 30 };
-ninjaOne = []; // this is allowed because arrays are objects in javascript
-var ninjaTwo;
-ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
+var mixedArray = [1, "string", false];
+// mixedArray = 'fish' // error - can't assign a string to an array of numbers
+var scrotum;
+scrotum = { name: "Scrotum", strength: 100, isVillain: false };
--- sandbox.ts
+++ sandbox.ts
@@ -1,36 +1,8 @@
-// explicit types
 
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+let mixedArray: any[] = [1, "string", false];
 
-// you can declare types if you don't have a value for them yet
+// mixedArray = 'fish' // error - can't assign a string to an array of numbers
 
-//arrays 
-let ninjas: string[] = [];
+let scrotum: { name: string, strength: number, isVillain: boolean } 
 
-// ninjas = [10, 23] // this will throw an error because ninjas is an array of strings
-ninjas.push('shaun');
-
-// union types
-let mixed: (string | number )[] = [];
-mixed.push('hello');
-mixed.push(20); 
-// mixed.push(false); // this will throw an error because mixed is an array of strings and numbers 
-
-let uid: string | number;
-uid = '123';
-uid = 123;
-
-// objects
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
-ninjaOne = []; // this is allowed because arrays are objects in javascript
-
-let ninjaTwo: {
-    name: string,
-    age: number,
-    beltColor: string
-};
-
-ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
+scrotum = { name: "Scrotum", strength: 100, isVillain: false }
